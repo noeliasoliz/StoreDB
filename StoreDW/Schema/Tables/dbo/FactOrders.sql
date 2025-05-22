97257817cf95a0efc84f4931cfaa52bb11c7006d
Noelia Andrea Soliz Coca <noeliasoliz@outlook.com>
2025-05-21 22:35:07 -0400

    Actualización de StoreDW: cambios en FactOrders y modelo

--- StoreDW/Schema/Tables/dbo/FactOrders.sql
+++ StoreDW/Schema/Tables/dbo/FactOrders.sql
@@ -10,4 +10,16 @@
     [Quantity] SMALLINT NOT NULL, 
     [Discount] REAL NOT NULL, 
     CONSTRAINT [PK_FactOrders] PRIMARY KEY ([ProductSK], [OrderID]) 
-)
+);
+GO
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimCustomer] FOREIGN KEY([CustomerSK]) REFERENCES [dbo].[DimCustomer] ([CustomerSK]);
+GO
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimEmployee] FOREIGN KEY([EmployeeSK] ) REFERENCES [dbo].[DimEmployee] ([EmployeeSK]);
+GO
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimProduct] FOREIGN KEY([ProductSK] ) REFERENCES [dbo].[DimProduct] ([ProductSK]);
+GO
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimShipper] FOREIGN KEY([ShipViaSK] ) REFERENCES [dbo].[DimShippers] ([ShipperSK]);
+GO
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate] FOREIGN KEY([OrderDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]);
+GO
+
